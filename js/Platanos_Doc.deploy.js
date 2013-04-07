cd4349c1d48e4b20184d342bfeece8baa56bdb84
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2013-04-07 04:38:14 +0300

    ok

--- js/Platanos_Doc.deploy.js
+++ js/Platanos_Doc.deploy.js
@@ -3,7 +3,6 @@ smalltalk.addClass('Doc', smalltalk.HashedCollection, [], 'Platanos_Doc');
 smalltalk.addMethod(
 smalltalk.method({
 selector: "addAsc:",
-category: 'not yet classified',
 fn: function (aSha1){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -13,65 +12,45 @@ if(! smalltalk.assert($1)){
 _st(_st(self)._at_("asc"))._add_(aSha1);
 };
 return self}, function($ctx1) {$ctx1.fill(self,"addAsc:",{aSha1:aSha1},smalltalk.Doc)})},
-args: ["aSha1"],
-source: "addAsc: aSha1\x0a\x0a((self at:'asc') includes: aSha1) ifFalse:[(self at:'asc') add:aSha1].",
-messageSends: ["ifFalse:", "add:", "at:", "includes:"],
-referencedClasses: []
-}),
+messageSends: ["ifFalse:", "add:", "at:", "includes:"]}),
 smalltalk.Doc);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "ascDetect:",
-category: 'not yet classified',
 fn: function (aBlock){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
 _st(_st(self)._at_("asc"))._detect_(aBlock);
 return self}, function($ctx1) {$ctx1.fill(self,"ascDetect:",{aBlock:aBlock},smalltalk.Doc)})},
-args: ["aBlock"],
-source: "ascDetect: aBlock\x0a\x0a(self at:'asc') detect: aBlock.",
-messageSends: ["detect:", "at:"],
-referencedClasses: []
-}),
+messageSends: ["detect:", "at:"]}),
 smalltalk.Doc);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "ascDetect:ifNone:",
-category: 'not yet classified',
 fn: function (aBlock,anotherBlock){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
 _st(_st(self)._at_("asc"))._detect_ifNone_(aBlock,anotherBlock);
 return self}, function($ctx1) {$ctx1.fill(self,"ascDetect:ifNone:",{aBlock:aBlock,anotherBlock:anotherBlock},smalltalk.Doc)})},
-args: ["aBlock", "anotherBlock"],
-source: "ascDetect: aBlock ifNone: anotherBlock\x0a\x0a(self at:'asc') detect: aBlock ifNone:anotherBlock.",
-messageSends: ["detect:ifNone:", "at:"],
-referencedClasses: []
-}),
+messageSends: ["detect:ifNone:", "at:"]}),
 smalltalk.Doc);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "ascDo:",
-category: 'not yet classified',
 fn: function (aBlock){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
 _st(_st(self)._at_("asc"))._do_(aBlock);
 return self}, function($ctx1) {$ctx1.fill(self,"ascDo:",{aBlock:aBlock},smalltalk.Doc)})},
-args: ["aBlock"],
-source: "ascDo: aBlock\x0a\x0a(self at:'asc') do: aBlock.",
-messageSends: ["do:", "at:"],
-referencedClasses: []
-}),
+messageSends: ["do:", "at:"]}),
 smalltalk.Doc);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "initialize",
-category: 'not yet classified',
 fn: function (){
 var self=this;
 function $Set(){return smalltalk.Set||(typeof Set=="undefined"?nil:Set)}
@@ -79,17 +58,12 @@ return smalltalk.withContext(function($ctx1) {
 smalltalk.HashedCollection.fn.prototype._initialize.apply(_st(self), []);
 _st(self)._at_put_("asc",_st($Set())._new());
 return self}, function($ctx1) {$ctx1.fill(self,"initialize",{},smalltalk.Doc)})},
-args: [],
-source: "initialize\x0a\x0asuper initialize.\x0aself at:'asc' put: (Set new).",
-messageSends: ["initialize", "at:put:", "new"],
-referencedClasses: ["Set"]
-}),
+messageSends: ["initialize", "at:put:", "new"]}),
 smalltalk.Doc);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "myPosition:",
-category: 'not yet classified',
 fn: function (aDocGraph){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -107,18 +81,13 @@ throw $early=[$2];
 return self}
 catch(e) {if(e===$early)return e[0]; throw e}
 }, function($ctx1) {$ctx1.fill(self,"myPosition:",{aDocGraph:aDocGraph},smalltalk.Doc)})},
-args: ["aDocGraph"],
-source: "myPosition: aDocGraph\x0a\x0aaDocGraph lines keysAndValuesDo:[:key :value|  (value includesKey: (self at:'sha1')) ifTrue:[^key.]].",
-messageSends: ["keysAndValuesDo:", "ifTrue:", "includesKey:", "at:", "lines"],
-referencedClasses: []
-}),
+messageSends: ["keysAndValuesDo:", "ifTrue:", "includesKey:", "at:", "lines"]}),
 smalltalk.Doc);
 
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "json:",
-category: 'not yet classified',
 fn: function (aJsonString){
 var self=this;
 var a,b,temp;
@@ -159,17 +128,12 @@ _st(a)._at_put_("psha1",temp);
 $5=a;
 return $5;
 }, function($ctx1) {$ctx1.fill(self,"json:",{aJsonString:aJsonString,a:a,b:b,temp:temp},smalltalk.Doc.klass)})},
-args: ["aJsonString"],
-source: "json: aJsonString\x0a\x0a|a b temp|\x0a\x0aa := self new.\x0ab := JSON parse: aJsonString.\x0aa at:'sha1' put: (b at: 'sha1'). \x22the sha1 of all all the below\x22\x0atemp := (b at: 'summary').\x0atemp ifNotNil: [a at:'summary' put: temp].\x0atemp := (b at: 'csummary').\x0atemp ifNotNil: [a at:'csummary' put: temp].\x0atemp := (b at: 'content').\x0atemp ifNotNil: [a at:'content' put: temp].\x0atemp := (b at: 'psha1').\x0atemp ifNotNil: [a at:'psha1' put: temp].   \x22an Array of the previous sha1\x22\x0a\x0a^a",
-messageSends: ["new", "parse:", "at:put:", "at:", "ifNotNil:"],
-referencedClasses: ["JSON"]
-}),
+messageSends: ["new", "parse:", "at:put:", "at:", "ifNotNil:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "jsono:",
-category: 'not yet classified',
 fn: function (aJson){
 var self=this;
 var a,b,temp;
@@ -209,17 +173,12 @@ _st(a)._at_put_("psha1",temp);
 $5=a;
 return $5;
 }, function($ctx1) {$ctx1.fill(self,"jsono:",{aJson:aJson,a:a,b:b,temp:temp},smalltalk.Doc.klass)})},
-args: ["aJson"],
-source: "jsono: aJson\x0a\x0a|a b temp|\x0a\x0aa := self new.\x0ab := aJson.\x0aa at:'sha1' put: (b at: 'sha1'). \x22the sha1 of all all the below\x22\x0atemp := (b at: 'summary').\x0atemp ifNotNil: [a at:'summary' put: temp].\x0atemp := (b at: 'csummary').\x0atemp ifNotNil: [a at:'csummary' put: temp].\x0atemp := (b at: 'content').\x0atemp ifNotNil: [a at:'content' put: temp].\x0atemp := (b at: 'psha1').\x0atemp ifNotNil: [a at:'psha1' put: temp].   \x22an Array of the previous sha1\x22\x0a\x0a^a",
-messageSends: ["new", "at:put:", "at:", "ifNotNil:"],
-referencedClasses: []
-}),
+messageSends: ["new", "at:put:", "at:", "ifNotNil:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "sha1:csummary:psha1:",
-category: 'not yet classified',
 fn: function (aSha1,aCSummary,aPsha1){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -227,17 +186,12 @@ var $1;
 $1=_st(self)._sha1_summary_content_csummary_psha1_(aSha1,nil,nil,aCSummary,aPsha1);
 return $1;
 }, function($ctx1) {$ctx1.fill(self,"sha1:csummary:psha1:",{aSha1:aSha1,aCSummary:aCSummary,aPsha1:aPsha1},smalltalk.Doc.klass)})},
-args: ["aSha1", "aCSummary", "aPsha1"],
-source: "sha1: aSha1 csummary: aCSummary psha1: aPsha1\x0a\x0a^ self sha1: aSha1 summary: nil content: nil csummary: aCSummary psha1: aPsha1",
-messageSends: ["sha1:summary:content:csummary:psha1:"],
-referencedClasses: []
-}),
+messageSends: ["sha1:summary:content:csummary:psha1:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "sha1:summary:",
-category: 'not yet classified',
 fn: function (aSha1,aSummary){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -245,17 +199,12 @@ var $1;
 $1=_st(self)._sha1_summary_content_csummary_psha1_(aSha1,aSummary,nil,nil,nil);
 return $1;
 }, function($ctx1) {$ctx1.fill(self,"sha1:summary:",{aSha1:aSha1,aSummary:aSummary},smalltalk.Doc.klass)})},
-args: ["aSha1", "aSummary"],
-source: "sha1: aSha1 summary: aSummary\x0a\x0a^ self sha1: aSha1 summary: aSummary content: nil csummary: nil psha1: nil",
-messageSends: ["sha1:summary:content:csummary:psha1:"],
-referencedClasses: []
-}),
+messageSends: ["sha1:summary:content:csummary:psha1:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "sha1:summary:content:",
-category: 'not yet classified',
 fn: function (aSha1,aSummary,aContent){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -263,17 +212,12 @@ var $1;
 $1=_st(self)._sha1_summary_content_csummary_psha1_(aSha1,aSummary,aContent,nil,nil);
 return $1;
 }, function($ctx1) {$ctx1.fill(self,"sha1:summary:content:",{aSha1:aSha1,aSummary:aSummary,aContent:aContent},smalltalk.Doc.klass)})},
-args: ["aSha1", "aSummary", "aContent"],
-source: "sha1: aSha1 summary: aSummary content: aContent\x0a\x0a^ self sha1: aSha1 summary: aSummary content: aContent csummary: nil psha1: nil",
-messageSends: ["sha1:summary:content:csummary:psha1:"],
-referencedClasses: []
-}),
+messageSends: ["sha1:summary:content:csummary:psha1:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "sha1:summary:content:csummary:psha1:",
-category: 'not yet classified',
 fn: function (aSha1,aSummary,aContent,aCSummary,aPsha1){
 var self=this;
 var a;
@@ -308,17 +252,12 @@ _st(a)._at_put_("psha1",aPsha1);
 $5=a;
 return $5;
 }, function($ctx1) {$ctx1.fill(self,"sha1:summary:content:csummary:psha1:",{aSha1:aSha1,aSummary:aSummary,aContent:aContent,aCSummary:aCSummary,aPsha1:aPsha1,a:a},smalltalk.Doc.klass)})},
-args: ["aSha1", "aSummary", "aContent", "aCSummary", "aPsha1"],
-source: "sha1: aSha1 summary: aSummary content: aContent csummary: aCSummary psha1: aPsha1\x0a\x0a|a|\x0a\x0aa := self new.\x0aa at:'sha1' put: aSha1. \x22the sha1 of all all the below\x22\x0aaSummary ifNotNil: [a at:'summary' put: aSummary].\x0aaCSummary ifNotNil: [a at:'csummary' put: aCSummary].\x0aaContent ifNotNil: [a at:'content' put: aContent].\x0aaPsha1 ifNotNil: [a at:'psha1' put: aPsha1].   \x22an Array of the previous sha1\x22\x0a\x0a^a",
-messageSends: ["new", "at:put:", "ifNotNil:"],
-referencedClasses: []
-}),
+messageSends: ["new", "at:put:", "ifNotNil:"]}),
 smalltalk.Doc.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
 selector: "sha1:summary:psha1:",
-category: 'not yet classified',
 fn: function (aSha1,aSummary,aPsha1){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
@@ -326,11 +265,7 @@ var $1;
 $1=_st(self)._sha1_summary_content_csummary_psha1_(aSha1,aSummary,nil,nil,aPsha1);
 return $1;
 }, function($ctx1) {$ctx1.fill(self,"sha1:summary:psha1:",{aSha1:aSha1,aSummary:aSummary,aPsha1:aPsha1},smalltalk.Doc.klass)})},
-args: ["aSha1", "aSummary", "aPsha1"],
-source: "sha1: aSha1 summary: aSummary psha1: aPsha1\x0a\x0a^ self sha1: aSha1 summary: aSummary content: nil csummary: nil psha1: aPsha1",
-messageSends: ["sha1:summary:content:csummary:psha1:"],
-referencedClasses: []
-}),
+messageSends: ["sha1:summary:content:csummary:psha1:"]}),
 smalltalk.Doc.klass);
 
 
